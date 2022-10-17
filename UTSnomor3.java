db6795e57b28990201aec75d90b4d149cbe680e5
Hieronimus Bonfillo Putra Angoro <112527221+Bandar-Tsuki@users.noreply.github.com>
2022-10-17 21:35:34 +0700

    Update UTSnomor3.java

--- UTSnomor3.java
+++ UTSnomor3.java
@@ -4,14 +4,14 @@ public class UTSnomor3
     static int inputangka, sum;
     static {sum = 0;}
     static Scanner jeki = new Scanner(System.in);
-    static void angka()
+    static void inputangka()
     {   inputangka = jeki.nextInt();
         sum += inputangka;
     }
     public static void main(String[] args) {
         System.out.println("Input 10 Bilangan");
         for (int i=1;i<=10;i++){
-            System.out.print("angka ke " + i +" = ");inputangka();}
+            System.out.print("angka ke " + i +" = "); inputangka();}
         System.out.println("Hasil dari penjumlahan 10 angka diatas : " + sum);
     }
 }
