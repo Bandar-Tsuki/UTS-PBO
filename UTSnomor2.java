8054e5397ca2226de5fdd3aaf330329fa4b96a9b
Hieronimus Bonfillo Putra Angoro <112527221+Bandar-Tsuki@users.noreply.github.com>
2022-10-17 21:18:29 +0700

    Update UTSnomor2.java

--- UTSnomor2.java
+++ UTSnomor2.java
@@ -2,16 +2,16 @@ import java.util.Scanner;
 
 public class UTSnomor2
 {
-    static double tinggiMeter(double tinggi) {return Math.abs(tinggi/100);}
-    static double hitungBMI(double berat, double tinggi) {return Math.abs(berat / (tinggiMeter(tinggi)  * tinggiMeter(tinggi))); }
+    static double tinggi_badanMeter(double tinggi_badan) {return Math.abs(tinggi_badan/100);}
+    static double hitungBMI(double berat_badan, double tinggi_badan) {return Math.abs(berat_badan / (tinggi_badanMeter(tinggi_badan)  * tinggi_badanMeter(tinggi_badan))); }
     private String bmi;
     public UTSnomor2(double hitungBMI){
         if ( hitungBMI >= 18.5 && hitungBMI <= 22.9 )
-        {this.bmi = "Underweight";}
+        {this.bmi = "Ideal";}
         else if ( hitungBMI >= 22.9 ) {
             this.bmi = "Overweight";}
         else if ( hitungBMI <= 18.5 )
-        {this.bmi = "Overweight";
+        {this.bmi = "Underweight";
         }
     }
     public String BMITotal(){
@@ -20,14 +20,14 @@ public class UTSnomor2
     public static void main(String[] args) {
         Scanner clara = new Scanner(System.in);
         System.out.print("Input Berat Badan Anda (KG) = ");
-        double berat = clara.nextInt();
+        double berat_badan = clara.nextInt();
         System.out.print("Input Tinggi Badan Anda (CM) = ");
-        double tinggi = clara.nextInt();
-        System.out.print("Tinggi Anda Dalam Meter = " + tinggiMeter(tinggi));
+        double tinggi_badan = clara.nextInt();
+        System.out.print("Tinggi Anda Dalam Meter = " + tinggi_badanMeter(tinggi_badan));
         System.out.println(" ");
         System.out.println("Hasil : ");
-        UTSnomor2 G = new UTSnomor2(hitungBMI(berat, tinggi));
-        System.out.println("Point BMI Anda: " + hitungBMI(berat, tinggi));
-        System.out.println("Nilai Huruf kamu: " + G.BMITotal());
+        UTSnomor2 G = new UTSnomor2(hitungBMI(berat_badan, tinggi_badan));
+        System.out.println("Point BMI Anda: " + hitungBMI(berat_badan, tinggi_badan));
+        System.out.println("Pernyataan Berat Badan Anda: " + G.BMITotal());
     }
 }
